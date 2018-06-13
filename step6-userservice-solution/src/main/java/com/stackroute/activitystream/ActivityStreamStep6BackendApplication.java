be27b7396a4ab40b3b1bc3c8662baeba124b40d7
dmfullstack <metkaridinesh@gmail.com>
2018-06-13 17:08:54 +0530

    ic

---
+++ step6-userservice-solution/src/main/java/com/stackroute/activitystream/ActivityStreamStep6BackendApplication.java
@@ -0,0 +1,12 @@
+package com.stackroute.activitystream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityStreamStep6BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityStreamStep6BackendApplication.class, args);
+	}
+}
